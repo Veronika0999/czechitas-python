39a8561412950c2e8e6333e58ea6be4221bf7470
Verča <vercakrivankova@seznam.cz>
2023-03-29 19:28:55 +0200

    ukol1

---
+++ ukol1.py
@@ -0,0 +1,32 @@
+#Domácí úkol - 1.úkol:
+
+jmeno = "Verca"
+print(f"{jmeno}@czechitas.cz")
+
+
+jmeno = input("Zadej své jméno: ")
+if jmeno =="Verca":
+    print(f"{jmeno}@czechitas.cz")
+else:
+    print("Špatně, zkus to znovu.")
+
+
+#Nepovinný bonus:
+
+jmeno_a_prijmeni = input("Jak se jmenuješ? ")
+
+print(jmeno_a_prijmeni.upper())
+print(jmeno_a_prijmeni.lower())
+print(jmeno_a_prijmeni.title())
+
+#inicialy 
+jmeno = input("Vlož své křesní jméno: ")
+prijmeni = input("Vlož své příjmení: ")
+inicialy = (jmeno[0] +"." + prijmeni[0]+".")
+print(inicialy.upper())
+
+if len(jmeno) <= 5:
+    inicialy = (jmeno[0] +"." + prijmeni[0]+".")
+    print(inicialy.upper())
+else:
+    print(jmeno + " "+ prijmeni)
