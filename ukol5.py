2d85934dab51ecc0556c00235d8dad94c8a729d5
Veronika Křivánková <129206296+Veronika0999@users.noreply.github.com>
2023-05-03 20:25:04 +0200

    Update ukol5.py

--- ukol5.py
+++ ukol5.py
@@ -1,4 +1,4 @@
-tempetature = [
+temperature = [
     [2.1, 5.2, 6.1, -0.1],
     [2.2, 4.8, 5.6, -1.0],
     [3.3, 6.5, 5.9, 1.2],
@@ -9,17 +9,22 @@ tempetature = [
 ]
 
 #Vytvořte seznam průměrných teplot pro každý den
-average = [round(sum(y)) for y in tempetature]
-print(f"Seznam průměrných teplot pro každý den: {average}")
+import statistics
+average = []
+
+for day in temperature:
+    average_statistics = statistics.mean(day)
+    average.append(average_statistics)
+print(average)
 
 #Vytvořte seznam ranních teplot
-morning_temperature = [x[0] for x in tempetature]
+morning_temperature = [x[0] for x in temperature]
 print(f"Seznam ranních teplot: {morning_temperature}")
 
 #Vytvořte seznam nočních teplot
-night_temperature = [z[3] for z in tempetature]
+night_temperature = [z[3] for z in temperature]
 print(f"Seznam nočních teplot: {night_temperature}")
 
 #Vytvořte seznam dvouprvkových seznamů obsahujících pouze polední a noční teplotu
-afternoon_and_night_temperature = [(q[1], q[3]) for q in tempetature]
+afternoon_and_night_temperature = [(q[1], q[3]) for q in temperature]
 print(f"Seznam obsahující polední a noční teplotu: {afternoon_and_night_temperature}")
